d73453932934d67b17e16c62d6ec828dfb933034
William B. Montini <williambm@gmail.com>
2021-11-03 09:39:39 -0300

    Initiated the code of Event Resource

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getOne(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+
+            return new EventDTO(entity);
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException("Can´t make an update because Event resource id: "+id+" not found");
+        }
+    }
+
+    private void copyDtoToEntity(EventDTO dto, Event entity) {
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(cityRepository.getOne(dto.getCityId()));
+    }
+
+}
